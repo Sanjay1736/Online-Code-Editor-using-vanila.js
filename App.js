4cdf4d546c446d78f6b1d2899cbbc862841745de
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2023-01-13 00:10:10 +0530

    Changes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// creating a run funtion
 function run() {
   let htmlCode = document.querySelector(".container  #htmlCode").value;
   let cssCode =
@@ -6,10 +7,14 @@ function run() {
     "</style>";
   let jsCode = document.querySelector(".container #javaScript").value;
   let output = document.querySelector(".iframe-container #viewer");
-  console.log(htmlCode, cssCode, jsCode, output);
-  output.contentDocument.body.innerHTML = htmlCode + cssCode;
+//   console.log(htmlCode, cssCode, jsCode, output);
+    // Appending the value inside the Html&Code to iframe means ouput innnerHTML.
+    output.contentDocument.body.innerHTML = htmlCode + cssCode;
+    //for evaling jsCode we are eval function inside javaScript by ouput.contentWindow.eval and passing jsCode as a argument
   output.contentWindow.eval(jsCode);
 }
+//we are making documnet objects in global
+//by using addEventLisner we are passing type of of that listner to do and passing the first class function  
 document.querySelector(".container #htmlCode").addEventListener("keyup", run);
 document.querySelector(".container #cssCode").addEventListener("keyup", run);
 document.querySelector(".container #javaScript").addEventListener("keyup", run);
---
+++ README.md
@@ -0,0 +1,2 @@
+# OnlineCode Editor Using Vanila.js
+
